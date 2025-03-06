941f0f5821cffc54591f5937672b74b52e5b9e8d
Samuel Gitau <amsamgitau@gmail.com>
2025-03-06 12:13:43 +0300

    modified files

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePage from './Home/HomePage'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App
